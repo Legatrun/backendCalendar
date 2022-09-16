acd960d0976f8ee3b97868de3e24419ba79bb433
Joaquim Merlo <joaquimmerlo1@gmail.com>
2022-09-16 17:42:22 -0400

    path agregado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const cors = require('cors')
 const { dbConnection } = require('./database/config');
-
+const path = require('path')
 
 const app = express();
 
@@ -21,6 +21,9 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
